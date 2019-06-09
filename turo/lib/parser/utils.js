65cec2c5e6d797e08463198d23b1e575d97d815c
James Hugman <james@hugman.tv>
2019-06-10 00:18:02 +0100

    Add parser/utils.js

---
+++ turo/lib/parser/utils.js
@@ -0,0 +1,42 @@
+export class ParseError extends Error {
+  constructor (observed, expected) {
+    this._expectedTypes = expected;
+    this._observed = observed;
+  }
+}
+
+export function optional (lex, ...types) {
+  if (types.length === 0 && typeof lex === 'function') {
+    return lex();
+  }
+  const type = lex.peek().type;
+  if (types.indexOf(type) >= 0) {
+    return lex.next();
+  }
+}
+
+export function consume(lex, ...types) {
+  const token = optional(lex, ...types);
+  if (!token) {
+    throw new ParseError(lex.peek().match, types);
+  }
+  return token;
+}
+
+export function zeroOrMore (lex, ...types) {
+  const array = [];
+  let token;
+  while (token = optional(lex, ...types)) {
+    array.push(token);
+  }
+  return array;
+}
+
+export function oneOrMore (lex, ...types) {
+  const tokens = zeroOrMore(lex, ...types);
+  if (tokens.length === 0) {
+    throw new ParseError(lex.peek().match, types);
+  }
+
+  return tokens;
+}

af5bba16d9a753f0bf1d7d2c5dcba07dfc9e5bbc
Julian Krispel-Samsel <julian@reactrocket.com>
2019-07-09 08:31:45 +0100

    remove turo-web, turo-server, turo-react

--- turo-react/scripts/watch.js
+++
@@ -1,5 +0,0 @@
-const base = require('./build.js')
-
-module.exports = Object.assign(base, {}, {
-  watch: true,
-})
--- turo-react/src/Lang.js
+++
@@ -1,5 +0,0 @@
-import lang from '../../lang_en.json';
-
-export default function({ children }) {
-  return lang[children] || children
-}
